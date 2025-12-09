9845c84c8873f89bc25e16fcd9f33c038b119003
TeeToe10 <urmomha6@gmail.com>
2025-12-09 15:29:48 +0000

    work on loops

--- 1_loops.py
+++ 1_loops.py
@@ -4,23 +4,44 @@ fruits = ["apple", "banana", "cherry", "date"]
 
 # Challenge:
 # Use a for loop to print each fruit on a new line.
-# print(fruits[0])
-# print(fruits[1])
-# print(fruits[2])
-# print(fruits[3])
+print(fruits[0])
+print(fruits[1])
+print(fruits[2])
 for fruit in fruits:
     print(fruit)
+# i just worked with loops
+
 # Given a list of school subjects:
 subjects = ["Math", "Science", "History", "Art"]
 for subject in subjects:
     print(subject)
+for subject in subjects:
+    if subject == "History":
+        break
+    print(subject)
+for subject in subjects:
+    if subject == "Science":
+        continue
+    print(subject)
+
+#  print out each subject but stop when you rewach "History"
 # Challenge:
 # Use a for loop and range to print each subject along with its index:
 # Example output: "Subject 0: Math"
-
+for i in range(len(subjects)):
+    print(f"Subject {i}: {subjects[i]}")
 
 # Given:
 numbers = [5, 10, 15, 20]
 
 # Challenge:
 # Use a for loop to add all the numbers and print the total.
+total = 0
+for number in numbers:
+    total += number
+print(total)
+# first time total = 0
+# second time total = 0 + 5
+# third time total = 5 + 10
+# fourth time total = 15 + 25
+# fifth time total = 30 + 20
--- 2_while_loops.py
+++ 2_while_loops.py
@@ -1,6 +1,48 @@
+# while loops = execute some code while some condition remains true
+# name = input("Enter your name: ") 
+
+
+# if name == "":
+#     print("You did not enter your name")
+# else: 
+#     print("Hello (name)") 
+# while name == "":
+#     print("You did not enter your name")
+#     name = input("Enter your name") 
+# print("Hello (name)")
+
+# age = int(input("Enter your age: ")) 
+
+# while age < 0:
+#     print("Age cam't be negative") 
+#     age = int(input("Enter your age: ")) 
+
+# food = input("Enter a food you like (q to quit): ") 
+# while not food == "q":
+#     food_list = []
+#     print(f"You like {food}")
+#     food_list.append(food)
+#     food = input("Enter another food you like (q to quit): ")
+
+# print("Bye")
+
+# num = int(input("Enter a # between 1 - 10: "))
+# while num < 1 or num > 10:
+#     print(f"(num) is not valid")
+#     num = int(input("Enter a # between 1 - 10: "))
+
+# print(f"Your number is {num}")
+
 # Given:
 colors = ["red", "blue", "green", "yellow", "purple"]
 
 # Challenge:
 # Use a while loop to print each color UNTIL you find "yellow".
 # Do NOT print "yellow" — stop before it.
+index = 0
+while index < len(colors):
+    if colors[index] == "yellow":
+          break
+    print(colors[index])
+    index += 1 #increment the index to avoid infinite loop
+    
